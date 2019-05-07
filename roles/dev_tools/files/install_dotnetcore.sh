7a5ff7ce48c0500f3962f8a68639836b0dc8637b
Nathan Floor <nathan.floor@jembi.org>
2019-05-07 22:28:02 +0200

    cleaned up roles a bit, mostly leveraging snap packages

---
+++ roles/dev_tools/files/install_dotnetcore.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Register Microsoft key and feed
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+sudo mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/prod.list
+sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+sudo chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+sudo chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+
+# install dotnet core sdk
+sudo apt update
+sudo apt install -y apt-transport-https
+sudo apt install -y dotnet-sdk-2.1
+
+# test
+dotnet --version
